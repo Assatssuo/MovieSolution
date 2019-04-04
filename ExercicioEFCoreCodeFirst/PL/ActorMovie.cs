7618ea5f6dfb8359a62af5142bdaf931a0c6292d
Assatssuo <jorge_jhdeqm@hotmail.com>
2019-04-03 21:02:58 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioEFCoreCodeFirst/PL/ActorMovie.cs
@@ -0,0 +1,22 @@
+﻿using ExercicioEFCoreCodeFirst.PL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ExercicioEFCoreCodeFirst.PLk
+{
+    public class ActorMovie
+    {
+        [Key]
+        public int ActorMovieId { get; set; }
+        public String Character { get; set; }
+
+        public int ActorId { get; set; }
+        public virtual Actor Actor { get; set; }
+
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+        
+    }
+}
